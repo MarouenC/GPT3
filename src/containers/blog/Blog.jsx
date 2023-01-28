664797272ffd7b3e9eb393baacd1636f5b891d19
MarouenC <101365894+MarouenC@users.noreply.github.com>
2023-01-28 16:34:08 +0100

    final push

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-import './blog.css'
+import React from 'react';
+import {Article} from '../../components';
+import './blog.css';
+import {blog01, blog02, blog03,blog04,blog05} from './imports'
+const articleData =[
+  {
+  imgUrl : blog02,
+  date : "Jan 28, 2023",
+  title :"GPT-3 and Open  AI is the future. Let us exlore how it is?"
+  },
+  {
+    imgUrl : blog03,
+    date : "Jan 28, 2023",
+    title :"GPT-3 and Open  AI is the future. Let us exlore how it is?"
+  },
+  {
+    imgUrl : blog04,
+    date : "Jan 28, 2023",
+    title :"GPT-3 and Open  AI is the future. Let us exlore how it is?"
+  },
+  {
+    imgUrl : blog05,
+    date : "Jan 28, 2023",
+    title :"GPT-3 and Open  AI is the future. Let us exlore how it is?"
+  }
+]
+
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id="blog">
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container__a'>
+          <Article imgUrl={blog01} date="Jan 28, 2023"  title ="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+        <div className='gpt3__blog-container__b'>
+        {articleData.map((article,index)=>(
+          <Article imgUrl={article.imgUrl} date={article.date} title={article.title} key={index + article.title} />
+        ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './possibility.css'
+import possibility from '../../assets/possibility.png'
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id="possibility">
+     <div className='gpt3__possibility-image'>
+      <img src={possibility} alt="possibilities" />
+     </div>
+     <div className='gpt3__possibilty-description'>
+      <a href='#'>Request Early Access to Get Started</a>
+      <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <a href='#'>Request Early Access to Get Started</a>
+     </div>
+    </div>
   )
 }
 
